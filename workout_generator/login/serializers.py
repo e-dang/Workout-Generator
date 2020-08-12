8a653fb2153df8c5bc5c1b869f43acc0406f1577
Eric Dang <edang830@gmail.com>
2020-08-12 19:46:48 +0000

    Hash password in in UserSerializer

--- workout_generator/login/serializers.py
+++ workout_generator/login/serializers.py
@@ -6,3 +6,12 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'id', 'username', 'first_name', 'last_name', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
