a056b6f7682b10617366bf493b6e851f164ef5ae
Eric Dang <edang830@gmail.com>
2020-08-13 04:54:41 +0000

    Implement unit tests for password reset api

--- workout_generator/users/tests/test_api.py
+++ workout_generator/users/tests/test_api.py
@@ -3,6 +3,7 @@ import pytest
 from rest_framework.reverse import reverse
 from rest_framework.authtoken.models import Token
 from users.models import User
+from django.core import mail
 
 
 def invalidate_credentials(api_client):
@@ -292,3 +293,94 @@ def test_user_change_password_fail_invalid_input(auto_login_user, test_password,
 
     assert resp.status_code == 400
     assert error_field in resp.data
+
+
+@pytest.mark.parametrize('auto_login_user, logout', [
+    (None, True),
+    (None, False)
+], indirect=['auto_login_user'], ids=['logged out', 'logged in'])
+@pytest.mark.django_db
+def test_password_reset_request(auto_login_user, logout):
+    url = reverse('rest_password_reset')
+    api_client, user = auto_login_user()
+    if logout:
+        api_client.credentials()
+
+    resp = api_client.post(url, {'email': user.email})
+
+    assert resp.status_code == 200
+    assert resp.context['user'].email == user.email
+    assert 'uid' in resp.context
+    assert 'token' in resp.context
+
+
+@pytest.mark.django_db
+def test_password_reset_request_fail_mismatch_credentials(auto_login_user):
+    url = reverse('rest_password_reset')
+    api_client, user = auto_login_user()
+    invalidate_credentials(api_client)
+
+    resp = api_client.post(url, {'email': user.email})
+
+    assert resp.status_code == 401
+
+
+@pytest.mark.parametrize('auto_login_user, logout', [
+    (None, True),
+    (None, False)
+], indirect=['auto_login_user'], ids=['logged out', 'logged in'])
+@pytest.mark.django_db
+def test_password_reset_confirm(auto_login_user, logout):
+    url = reverse('rest_password_reset')
+    api_client, user = auto_login_user()
+    if logout:
+        api_client.credentials()
+    resp = api_client.post(url, {'email': user.email})
+    url = reverse('password_reset_confirm', kwargs={'token': resp.context['token'], 'uidb64': resp.context['uid']})
+    new_password = 'thisisanewpassword123'
+    data = {'new_password1': new_password, 'new_password2': new_password,
+            'token': resp.context['token'], 'uid': resp.context['uid']}
+
+    resp = api_client.post(url, data)
+
+    assert resp.status_code == 200
+    assert User.objects.get(email=user.email).check_password(new_password)
+
+
+@pytest.mark.django_db
+def test_password_reset_confirm_fail_mismatch_credentials(auto_login_user):
+    url = reverse('rest_password_reset')
+    api_client, user = auto_login_user()
+    resp = api_client.post(url, {'email': user.email})
+    url = reverse('password_reset_confirm', kwargs={'token': resp.context['token'], 'uidb64': resp.context['uid']})
+    new_password = 'thisisanewpassword123'
+    data = {'new_password1': new_password, 'new_password2': new_password,
+            'token': resp.context['token'], 'uid': resp.context['uid']}
+    invalidate_credentials(api_client)
+
+    resp = api_client.post(url, data)
+
+    assert resp.status_code == 401
+
+
+@pytest.mark.parametrize('auto_login_user, test_password, password1, password2, token, uid, error_field', [
+    (None, None, 'thisisanewpassword123', 'thisisanewpassword', None, None, 'new_password2'),
+    (None, None, 'thisisanewpassword123', 'thisisanewpassword123', 'WRONG_TOKEN', None, 'token'),
+    (None, None, 'thisisanewpassword123', 'thisisanewpassword123', None, 'WRONG_UID', 'uid'),
+],
+    indirect=['auto_login_user', 'test_password'],
+    ids=['mismatch passwords', 'wrong token', 'wrong uid'])
+@pytest.mark.django_db
+def test_password_reset_confirm_fail_invalid_input(auto_login_user, test_password, password1, password2, token, uid, error_field):
+    url = reverse('rest_password_reset')
+    api_client, user = auto_login_user()
+    resp = api_client.post(url, {'email': user.email})
+    url = reverse('password_reset_confirm', kwargs={'token': resp.context['token'], 'uidb64': resp.context['uid']})
+    data = {'new_password1': password1, 'new_password2': password2,
+            'token': token or resp.context['token'], 'uid': uid or resp.context['uid']}
+
+    resp = api_client.post(url, data)
+
+    assert resp.status_code == 400
+    assert error_field in resp.data
+    assert User.objects.get(email=user.email).check_password(test_password)
