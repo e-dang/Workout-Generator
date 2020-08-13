9629ace94727ede52ea18ed6ac26bb307003b43b
Eric Dang <edang830@gmail.com>
2020-08-13 00:57:34 +0000

    Implement endpoint for deleting authenticated user

--- workout_generator/users/urls.py
+++ workout_generator/users/urls.py
@@ -1,8 +1,10 @@
+from .views import UserDeleteView
 from django.urls import include, path
 from rest_auth.views import PasswordResetConfirmView
 
 urlpatterns = [
     path('', include('rest_auth.urls')),
+    path('delete/', UserDeleteView.as_view()),
     path('registration/', include('rest_auth.registration.urls')),
     path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm')
 ]
--- workout_generator/users/views.py
+++ workout_generator/users/views.py
@@ -1,9 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 from .models import User
 from .serializers import UserSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserDeleteView(generics.DestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        request.user.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
