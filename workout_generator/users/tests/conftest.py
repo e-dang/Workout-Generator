3b753cbf3a61c40f28ae0fcb1d4f051beaa780a0
Eric Dang <edang830@gmail.com>
2020-08-13 02:35:05 +0000

    Implement auto_login_user test fixture

--- workout_generator/users/tests/conftest.py
+++ workout_generator/users/tests/conftest.py
@@ -1,22 +1,43 @@
 import pytest
 
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
 
-# https://djangostars.com/blog/django-pytest-testing/
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
 @pytest.fixture
 def test_password():
     return 'strong-test-pass123'
 
 
 @pytest.fixture
-def test_email():
-    return 'JohnDoe@demo.com'
+def create_user(db, django_user_model, test_password):
+    """
+    Taken from https://djangostars.com/blog/django-pytest-testing/
+    """
 
-
-# https://djangostars.com/blog/django-pytest-testing/
-@pytest.fixture
-def create_user(db, django_user_model, test_email, test_password):
     def make_user(**kwargs):
-        kwargs['email'] = test_email
+        kwargs['email'] = kwargs.get('email', 'JohnDoe@demo.com')
         kwargs['password'] = test_password
         return django_user_model.objects.create_user(**kwargs)
     return make_user
+
+
+@pytest.fixture
+def auto_login_user(db, api_client, create_user):
+    """
+    Taken from https://djangostars.com/blog/django-pytest-testing/
+    """
+
+    def make_auto_login(user=None):
+        if user is None:
+            user = create_user()
+        token, _ = Token.objects.get_or_create(user=user)
+        api_client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
+        return api_client, user
+
+    return make_auto_login
