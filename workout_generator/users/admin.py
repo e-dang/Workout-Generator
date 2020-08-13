de75b53a4c8f97818283e3b30a85b9f1f627762a
Eric Dang <edang830@gmail.com>
2020-08-13 00:12:06 +0000

    Implement custom user admin

--- workout_generator/users/admin.py
+++ workout_generator/users/admin.py
@@ -1,3 +1,28 @@
-from django.contrib import admin
+from django.contrib.admin import site
+from django.contrib.auth import admin
 
-# Register your models here.
+from .forms import UserCreationForm, UserChangeForm
+from .models import User
+
+
+class UserAdmin(admin.UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    list_display = ('id', 'email', 'first_name', 'last_name')
+    list_filter = ()
+
+    fieldsets = (
+        (None, {'fields': ('email', 'first_name', 'last_name', 'password')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
+
+    search_fields = ('id', 'email', 'first_name', 'last_name')
+    ordering = ('id', 'email')
+
+
+site.register(User, UserAdmin)
