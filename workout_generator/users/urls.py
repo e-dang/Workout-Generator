2f84fa9e8016535dadbd02d0cae5291332c3e171
Eric Dang <edang830@gmail.com>
2020-08-13 03:32:17 +0000

    Given delete user api name of delete-user

--- workout_generator/users/urls.py
+++ workout_generator/users/urls.py
@@ -4,7 +4,7 @@ from rest_auth.views import PasswordResetConfirmView
 
 urlpatterns = [
     path('', include('rest_auth.urls')),
-    path('delete/', UserDeleteView.as_view()),
+    path('delete/', UserDeleteView.as_view(), name='delete-user'),
     path('registration/', include('rest_auth.registration.urls')),
     path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm')
 ]
