7ede47f32b0bc34186d8cc121eb8101c7ad9b027
Eric Dang <edang830@gmail.com>
2020-08-17 23:51:54 +0000

    Add names to muscles urls

--- workout_generator/muscles/urls.py
+++ workout_generator/muscles/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 import muscles.views as views
 
 urlpatterns = [
-    path('subportion/', views.MuscleSubPortionList.as_view()),
-    path('subportion/<str:pk>/', views.MuscleSubPortionDetail.as_view()),
-    path('muscle/', views.MuscleList.as_view()),
-    path('muscle/<str:pk>/', views.MuscleDetail.as_view()),
-    path('muscle-grouping/', views.MuscleGroupingList.as_view()),
-    path('muscle-grouping/<str:pk>/', views.MuscleGroupingDetail.as_view())
+    path('subportion/', views.MuscleSubPortionList.as_view(), name='muscle-subportion-list'),
+    path('subportion/<str:pk>/', views.MuscleSubPortionDetail.as_view(), name='muscle-subportion-detail'),
+    path('muscle/', views.MuscleList.as_view(), name='muscle-list'),
+    path('muscle/<str:pk>/', views.MuscleDetail.as_view(), name='muscle-detail'),
+    path('grouping/', views.MuscleGroupingList.as_view(), name='muscle-grouping-list'),
+    path('grouping/<str:pk>/', views.MuscleGroupingDetail.as_view(), name='muscle-grouping-detail')
 ]
