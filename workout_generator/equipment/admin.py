a784ac75b211421a291eda9467199ba44f4da493
Eric Dang <edang830@gmail.com>
2020-08-18 20:22:32 +0000

    Implement EquipmentAdmin admin model

--- workout_generator/equipment/admin.py
+++ workout_generator/equipment/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Equipment
 
-# Register your models here.
+
+class EquipmentAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Equipment, EquipmentAdmin)
