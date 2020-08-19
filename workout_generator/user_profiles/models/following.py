3b17f5ecd3eb52adae3b7a4f41aed187a508c757
Eric Dang <edang830@gmail.com>
2020-08-19 17:05:44 +0000

    Update Following model
    - Change field names to following_user and
      followed_user so the relationship between users
      is more clear
    - Add a Meta class to the model that requires the
      pair of Users to be unique together
    - Implement a __str__ method that indicates which
      User follows which

--- workout_generator/user_profiles/models/following.py
+++ workout_generator/user_profiles/models/following.py
@@ -3,6 +3,13 @@ from user_profiles.models.user_profile import UserProfile
 
 
 class Following(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    following = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    created = models.DateField(auto_now_add=True)
+    following_user = models.ForeignKey(UserProfile, related_name='following_user', on_delete=models.CASCADE)
+    followed_user = models.ForeignKey(UserProfile, related_name='followed_user', on_delete=models.CASCADE)
+    created = models.DateField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        unique_together = ('following_user', 'followed_user')
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'{str(self.following_user.user)} follows {str(self.followed_user.user)}'
