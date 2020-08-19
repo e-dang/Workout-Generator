abb0b7727a20e0096d7e741c40aafd2427e0d3d1
Eric Dang <edang830@gmail.com>
2020-08-19 19:44:47 +0000

    Apply changes from FollowRequest handling to tests

--- workout_generator/user_profiles/tests/test_user_profile.py
+++ workout_generator/user_profiles/tests/test_user_profile.py
@@ -1,7 +1,7 @@
 import pytest
 import os
 
-from user_profiles.models import UserProfile, Following
+from user_profiles.models import UserProfile, Following, FollowRequest
 
 
 @pytest.mark.django_db
@@ -11,7 +11,7 @@ def test_follow_user(create_user):
     profile1 = user1.profile
     profile2 = user2.profile
 
-    profile1.follow_user(profile2)
+    profile1._follow_profile(profile2)
 
     assert profile2 in profile1.following.all()
     assert profile2 not in profile1.followers.all()
@@ -36,11 +36,15 @@ def test_user_profile_auto_create(create_user):
 
 @pytest.mark.django_db
 def test_user_profile_deleted_on_user_delete(create_user):
-    user = create_user()
+    user1 = create_user()
+    user2 = create_user(email='JaneDoe@demo.com')
+    user2.profile.make_follow_request(user1.profile)
     user_profile_before = len(UserProfile.objects.all())
     following_before = len(Following.objects.all())
+    follow_request_before = len(FollowRequest.objects.all())
 
-    user.delete()
+    user1.delete()
 
     assert len(UserProfile.objects.all()) == user_profile_before - 1
     assert len(Following.objects.all()) == following_before - 1
+    assert len(FollowRequest.objects.all()) == follow_request_before - 1
