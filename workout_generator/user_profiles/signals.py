965b20094f3c8026b33fb2570eeba078be5e1c9b
Eric Dang <edang830@gmail.com>
2020-08-19 17:11:37 +0000

    Implement UserProfile signals
    - Implement create_user_profile() that auto
      creates a UserProfile each time a new User is
      created and automatically have that UserProfile
      follow the global User.
    - Implement save_user_profile() which auto
      updates a UserProfile.

--- workout_generator/user_profiles/signals.py
+++ workout_generator/user_profiles/signals.py
@@ -1,13 +1,17 @@
+import os
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from users.models import User
+
 from user_profiles.models import UserProfile
+from users.models import User
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        profile = UserProfile.objects.create(user=instance)
+        profile.follow_user(UserProfile.objects.get(user__email=os.environ.get('GLOBAL_EMAIL')))
 
 
 @receiver(post_save, sender=User)
