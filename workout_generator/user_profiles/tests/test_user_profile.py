d37e4c877326211e59757afe51ee04041372035f
Eric Dang <edang830@gmail.com>
2020-08-19 20:33:03 +0000

    Implement tests for accept_follower_requst()

--- workout_generator/user_profiles/tests/test_user_profile.py
+++ workout_generator/user_profiles/tests/test_user_profile.py
@@ -5,7 +5,12 @@ from user_profiles.models import UserProfile, Following, FollowRequest
 
 
 @pytest.mark.django_db
-def test_follow_user(create_user):
+def test_follow_profile(create_user):
+    """
+    Tests that the instance method _follow_profile() correctly places the profiles in the following field of each
+    profile.
+    """
+
     user1 = create_user()
     user2 = create_user(email='JaneDoe@demo.com')
     profile1 = user1.profile
@@ -20,7 +25,53 @@ def test_follow_user(create_user):
 
 
 @pytest.mark.django_db
+def test_accept_follower_request(create_user):
+    """
+    Tests that the instance method _accept_follower_request() correctly places the profiles in the following field of
+    each profile.
+    """
+
+    user1 = create_user()
+    user2 = create_user(email='JaneDoe@demo.com')
+    profile1 = user1.profile
+    profile2 = user2.profile
+    follower_request = FollowRequest.objects.create(requesting_profile=profile2, target_profile=profile1)
+
+    profile1._accept_follower_request(follower_request)
+
+    assert profile2 in profile1.followers.all()
+    assert profile2 not in profile1.following.all()
+    assert profile1 in profile2.following.all()
+    assert profile1 not in profile2.followers.all()
+
+
+@pytest.mark.django_db
+def test_accept_follower_request_fail(create_user):
+    """
+    Tests that the instance method _accept_follower_request() does not change anything if the target_profile on the
+    FollowRequest is not the same as the profile that issued the request.
+    """
+
+    user1 = create_user()
+    user2 = create_user(email='JaneDoe@demo.com')
+    profile1 = user1.profile
+    profile2 = user2.profile
+    follower_request = FollowRequest.objects.create(requesting_profile=profile1, target_profile=profile2)
+
+    profile1._accept_follower_request(follower_request)
+
+    assert profile2 not in profile1.followers.all()
+    assert profile2 not in profile1.following.all()
+    assert profile1 not in profile2.following.all()
+    assert profile1 not in profile2.followers.all()
+
+
+@pytest.mark.django_db
 def test_user_profile_auto_create(create_user):
+    """
+    Tests that when a new User is created that it automatically follows the global User instance.
+    """
+
     user = create_user()
 
     profile = UserProfile.objects.get(user=user.id)
@@ -36,9 +87,13 @@ def test_user_profile_auto_create(create_user):
 
 @pytest.mark.django_db
 def test_user_profile_deleted_on_user_delete(create_user):
+    """
+    Tests that when a User is deleted so is their associated UserProfile, Following entries, and FollowRequests.
+    """
+
     user1 = create_user()
     user2 = create_user(email='JaneDoe@demo.com')
-    user2.profile.make_follow_request(user1.profile)
+    FollowRequest.objects.create(requesting_profile=user1.profile, target_profile=user2.profile)
     user_profile_before = len(UserProfile.objects.all())
     following_before = len(Following.objects.all())
     follow_request_before = len(FollowRequest.objects.all())
