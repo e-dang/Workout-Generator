6422a08b1f79d6db0fc2069fbd0f6df021814483
Eric Dang <edang830@gmail.com>
2020-08-19 18:37:16 +0000

    Implement apply_attrs_to_model() and unittests

--- workout_generator/main/tests/test_utils.py
+++ workout_generator/main/tests/test_utils.py
@@ -1,5 +1,7 @@
-import pytest
 from collections import namedtuple
+
+import pytest
+
 from main import utils
 
 
@@ -8,9 +10,43 @@ from main import utils
     (namedtuple('Test', ''), {'x': 1, 'a': 1, '2': 1}, {}, {'x': 1, 'a': 1, '2': 1}),
     (None, {'x': 1, 'a': 1, '2': 1}, {}, {'x': 1, 'a': 1, '2': 1}),
     (namedtuple('Test', 'x y c'), {}, {}, {})
-])
+],
+    ids=['extra subset of kwargs', 'no attrs on model', 'model is None', 'empty kwargs'])
 def test_split_dict_on_model_attrs(model, kwargs, expected_kwargs, expected_extras):
     extras = utils.split_dict_on_model_attrs(model, kwargs)
 
     assert kwargs == expected_kwargs
     assert extras == expected_extras
+
+
+@pytest.mark.parametrize('model, kwargs, force', [
+    (namedtuple('Test', 'x y c'), {'z': 3}, False),
+    (namedtuple('Test', 'x y c'), {'x': 3}, True),
+    (namedtuple('Test', 'x y c'), {}, True),
+    (namedtuple('Test', 'x y c'), {}, False)
+],
+    ids=['add new attr', 'replace current attr', 'no new atts force', 'no new atts no force'])
+def test_apply_attrs_to_model(model, kwargs, force):
+    new_model = utils.apply_attrs_to_model(model, kwargs, force=force)
+
+    for key, value in kwargs.items():
+        attr = '_' + str(key)
+        assert hasattr(new_model, attr)
+        assert getattr(new_model, attr) == value
+
+    for attr in dir(model):
+        assert hasattr(new_model, attr)
+
+
+@pytest.mark.parametrize('model, kwargs, force', [
+    (None, {'z': 3}, False),
+    (None, {'z': 3}, True),
+    (namedtuple('Test', 'x y c'), {'x': 3}, False)
+],
+    ids=['model is None no force', 'model is None force', 'replace current attr no force'])
+def test_apply_attrs_to_model_fail(model, kwargs, force):
+    if model is not None:
+        model._x = None
+
+    with pytest.raises(ValueError):
+        _ = utils.apply_attrs_to_model(model, kwargs, force=force)
--- workout_generator/main/utils.py
+++ workout_generator/main/utils.py
@@ -14,3 +14,19 @@ def split_dict_on_model_attrs(model, kwargs):
             extras[key] = kwargs.pop(key)
 
     return extras
+
+
+def apply_attrs_to_model(model, kwargs, force=False):
+    if model is None:
+        raise ValueError('Cannot apply attributes to None')
+    elif len(kwargs) == 0:
+        return model
+
+    for key, value in kwargs.items():
+        attr = '_' + str(key)
+        if not hasattr(model, attr) or force:
+            setattr(model, attr, value)
+        else:
+            raise ValueError(f'The model already has the attribute {attr}. To override attributes set force = True.')
+
+    return model
