ce09aac1cbe1884628234e2560e00376b7c347b7
Eric Dang <edang830@gmail.com>
2020-08-19 22:36:34 +0000

    Implement unittests for UserProfile model methods

---
+++ workout_generator/user_profiles/tests/unittests/test_user_profiles.py
@@ -0,0 +1,112 @@
+import pytest
+import mock
+
+from user_profiles.models import UserProfile, Following, FollowRequest
+
+
+@pytest.mark.django_db
+def test_make_follow_request_public(create_user_no_follow):
+    """
+    Tests that the instance method make_follow_request() correctly places the profiles in the following field of each
+    profile when the followed User has public visibility.
+    """
+
+    user1 = create_user_no_follow()
+    user2 = create_user_no_follow(email='JaneDoe@demo.com', visibility=UserProfile.PUBLIC)
+    profile1 = user1.profile
+    profile2 = user2.profile
+
+    profile1.make_follow_request(profile2)
+
+    assert profile2 in profile1.following.all()
+    assert profile2 not in profile1.followers.all()
+    assert profile1 not in profile2.following.all()
+    assert profile1 in profile2.followers.all()
+
+
+@pytest.mark.django_db
+def test_make_follow_request_private(create_user_no_follow):
+    """
+    Tests that the instance method make_follow_request() correctly issues a FollowRequest when the followed User has
+    private visibility.
+    """
+
+    user1 = create_user_no_follow()
+    user2 = create_user_no_follow(email='JaneDoe@demo.com')
+    profile1 = user1.profile
+    profile2 = user2.profile
+
+    profile1.make_follow_request(profile2)
+
+    assert profile2 not in profile1.following.all()
+    assert profile2 not in profile1.followers.all()
+    assert profile1 not in profile2.following.all()
+    assert profile1 not in profile2.followers.all()
+    assert profile1 in profile2.follower_requests.all()
+    assert profile1 not in profile2.following_requests.all()
+    assert profile2 in profile1.following_requests.all()
+    assert profile2 not in profile1.follower_requests.all()
+
+
+@pytest.mark.parametrize('create_user_no_follow, accepted', [
+    (None, True),
+    (None, False)
+],
+    indirect=['create_user_no_follow'],
+    ids=['accepted', 'not accepted'])
+@pytest.mark.django_db
+def test_handle_follow_request(create_user_no_follow, accepted):
+    """
+    Tests that the instance method handle_follower_request() correctly places the profiles in the following field of
+    each profile when the request has been accepted, does nothing when the request has been rejected, and deletes
+    the request in either case.
+    """
+
+    user1 = create_user_no_follow()
+    user2 = create_user_no_follow(email='JaneDoe@demo.com')
+    profile1 = user1.profile
+    profile2 = user2.profile
+    follower_request = FollowRequest.objects.create(requesting_profile=profile2, target_profile=profile1)
+
+    profile1.handle_follow_request(follower_request, accepted)
+
+    if accepted:
+        assert profile2 in profile1.followers.all()
+        assert profile2 not in profile1.following.all()
+        assert profile1 in profile2.following.all()
+        assert profile1 not in profile2.followers.all()
+    else:
+        assert profile2 not in profile1.followers.all()
+        assert profile2 not in profile1.following.all()
+        assert profile1 not in profile2.following.all()
+        assert profile1 not in profile2.followers.all()
+
+    assert len(FollowRequest.objects.all()) == 0
+
+
+@pytest.mark.parametrize('create_user_no_follow, accepted', [
+    (None, True),
+    (None, False)
+],
+    indirect=['create_user_no_follow'],
+    ids=['accepted', 'not accepted'])
+@pytest.mark.django_db
+def test_handle_follow_request_invalid_request(create_user_no_follow, accepted):
+    """
+    Tests that the instance method handle_follower_request() does nothing when a follower_request not belonging to the
+    calling instance is passed to method.
+    """
+
+    user1 = create_user_no_follow()
+    user2 = create_user_no_follow(email='JaneDoe@demo.com')
+    profile1 = user1.profile
+    profile2 = user2.profile
+    follower_request = FollowRequest.objects.create(requesting_profile=profile1, target_profile=profile2)
+
+    profile1.handle_follow_request(follower_request, accepted)
+
+    assert profile2 not in profile1.followers.all()
+    assert profile2 not in profile1.following.all()
+    assert profile1 not in profile2.following.all()
+    assert profile1 not in profile2.followers.all()
+    assert len(FollowRequest.objects.all()) == 1
