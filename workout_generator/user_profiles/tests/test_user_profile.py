3bd353ca78d0501ae64a2f328ed8ecf1c2568832
Eric Dang <edang830@gmail.com>
2020-08-19 17:14:19 +0000

    Implement unittests for UserProfile and Following models

---
+++ workout_generator/user_profiles/tests/test_user_profile.py
@@ -0,0 +1,46 @@
+import pytest
+import os
+
+from user_profiles.models import UserProfile, Following
+
+
+@pytest.mark.django_db
+def test_follow_user(create_user):
+    user1 = create_user()
+    user2 = create_user(email='JaneDoe@demo.com')
+    profile1 = user1.profile
+    profile2 = user2.profile
+
+    profile1.follow_user(profile2)
+
+    assert profile2 in profile1.following.all()
+    assert profile2 not in profile1.followers.all()
+    assert profile1 not in profile2.following.all()
+    assert profile1 in profile2.followers.all()
+
+
+@pytest.mark.django_db
+def test_user_profile_auto_create(create_user):
+    user = create_user()
+
+    profile = UserProfile.objects.get(user=user.id)
+
+    global_user = UserProfile.objects.get(user__email=os.environ.get('GLOBAL_EMAIL'))
+    followers = global_user.followers.all()
+    assert user.profile == profile
+    assert len(profile.following.all()) == 1
+    assert len(profile.followers.all()) == 0
+    assert len(followers) == 2
+    assert profile in followers
+
+
+@pytest.mark.django_db
+def test_user_profile_deleted_on_user_delete(create_user):
+    user = create_user()
+    user_profile_before = len(UserProfile.objects.all())
+    following_before = len(Following.objects.all())
+
+    user.delete()
+
+    assert len(UserProfile.objects.all()) == user_profile_before - 1
+    assert len(Following.objects.all()) == following_before - 1
