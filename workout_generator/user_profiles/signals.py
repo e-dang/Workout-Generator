ed741b4529f8a5b001497623a5c861ee1c6703bb
Eric Dang <edang830@gmail.com>
2020-08-19 19:43:44 +0000

    Extract UserProfile attrs off of User instance

--- workout_generator/user_profiles/signals.py
+++ workout_generator/user_profiles/signals.py
@@ -5,13 +5,16 @@ from django.dispatch import receiver
 
 from user_profiles.models import UserProfile
 from users.models import User
+from main import utils
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        profile = UserProfile.objects.create(user=instance)
-        profile.follow_user(UserProfile.objects.get(user__email=os.environ.get('GLOBAL_EMAIL')))
+        extra_fields = [field.name for field in UserProfile._meta.get_fields()]
+        extra_values = utils.extract_extra_fields_from_model(instance, extra_fields)
+        profile = UserProfile.objects.create(user=instance, **extra_values)
+        profile.make_follow_request(UserProfile.objects.get(user__email=os.environ.get('GLOBAL_EMAIL')))
 
 
 @receiver(post_save, sender=User)
