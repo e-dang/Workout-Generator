953db0378efeb4aba8aac8136aad61c1aaea7fdc
Eric Dang <edang830@gmail.com>
2020-08-21 02:16:10 +0000

    Add name to UserProfile detail view

--- workout_generator/user_profiles/urls.py
+++ workout_generator/user_profiles/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from user_profiles.views import UserProfileDetail
 
 urlpatterns = [
-    path('<int:pk>/', UserProfileDetail.as_view())
+    path('<int:pk>/', UserProfileDetail.as_view(), name='profile-detail')
 ]
