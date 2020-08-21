4fe359d6414a9cd158281c9c63e1f23aeaa9c92c
Eric Dang <edang830@gmail.com>
2020-08-21 02:05:12 +0000

    Implement auto_login_profile fixture

--- workout_generator/user_profiles/tests/unittests/conftest.py
+++ workout_generator/user_profiles/tests/unittests/conftest.py
@@ -22,3 +22,14 @@ def create_user(db, django_user_model, test_password):
             return django_user_model.objects.create_user(**kwargs)
 
     return make_user
+
+
+@pytest.fixture
+def auto_login_profile(auto_login_user):
+    def convert_user_to_profile(**kwargs):
+        kwargs['weight'] = kwargs.get('weight', 160)
+        kwargs['height'] = kwargs.get('height', 60)
+        kwargs['bmi'] = kwargs.get('bmi', 12)
+        api_client, user = auto_login_user(**kwargs)
+        return api_client, user.profile
+    return convert_user_to_profile
