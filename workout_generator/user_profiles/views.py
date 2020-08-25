fd5d811b046927cb164f900ecff31a978f5952cf
Eric Dang <edang830@gmail.com>
2020-08-25 20:03:37 +0000

    Apply IsAdmin permission to the UserProfile view

--- workout_generator/user_profiles/views.py
+++ workout_generator/user_profiles/views.py
@@ -1,10 +1,10 @@
 from rest_framework.generics import RetrieveUpdateAPIView
 from user_profiles.models import UserProfile
 from user_profiles.serializers import UserProfileSerializer
-from user_profiles.permissions import IsOwner
+from user_profiles.permissions import IsOwner, IsAdmin
 
 
 class UserProfileDetail(RetrieveUpdateAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
-    permission_classes = [IsOwner]
+    permission_classes = [IsOwner | IsAdmin]
