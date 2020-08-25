81a397a8e2e4b699e3d110f51190e0c3cdf4a9eb
Eric Dang <edang830@gmail.com>
2020-08-25 19:49:17 +0000

    Implement IsAdmin permission and change subclasses
    - IsAdmin and IsOwner now both inherit from
      IsAuthenticated, which only implements the
      view permission. IsAdmin and IsOwner add on
      object level permissions corresponding to their
      name

--- workout_generator/user_profiles/permissions.py
+++ workout_generator/user_profiles/permissions.py
@@ -1,6 +1,11 @@
 from rest_framework import permissions
 
 
-class IsOwner(permissions.BasePermission):
+class IsOwner(permissions.IsAuthenticated):
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
+
+
+class IsAdmin(permissions.IsAuthenticated):
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_staff
