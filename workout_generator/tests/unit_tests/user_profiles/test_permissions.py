50591ecfb698ee3e0be938718aafad7be1fda8e0
Eric Dang <edang830@gmail.com>
2020-08-25 20:01:22 +0000

    Implement tests for IsAdmin and IsOwner
    - Implement object and view permissions for
      IsAdmin.
    - Implement view permissions for IsOwner, and
      rename to the object permission tests to be
      more explicit about the object level nature
      of those tests

--- workout_generator/tests/unit_tests/user_profiles/test_permissions.py
+++ workout_generator/tests/unit_tests/user_profiles/test_permissions.py
@@ -1,8 +1,9 @@
+import pytest
 import mock
-from user_profiles.permissions import IsOwner
+from user_profiles.permissions import IsOwner, IsAdmin
 
 
-def test_is_owner_has_permission():
+def test_is_owner_has_object_permission():
     mock_request = mock.MagicMock()
     mock_obj = mock.MagicMock()
     mock_view = mock.MagicMock()
@@ -15,7 +16,7 @@ def test_is_owner_has_permission():
     assert permissions.has_object_permission(mock_request, mock_view, mock_obj)
 
 
-def test_is_owner_doesnt_have_permission():
+def test_is_owner_doesnt_have_object_permission():
     mock_request = mock.MagicMock()
     mock_obj = mock.MagicMock()
     mock_view = mock.MagicMock()
@@ -27,3 +28,39 @@ def test_is_owner_doesnt_have_permission():
     mock_obj.user = mock_user2
 
     assert not permissions.has_object_permission(mock_request, mock_view, mock_obj)
+
+
+@pytest.mark.parametrize('is_admin', [
+    True,
+    False
+],
+    ids=['is admin', 'is not admin'])
+def test_is_admin_has_object_permission(is_admin):
+    mock_request = mock.MagicMock()
+    mock_view = mock.MagicMock()
+    mock_object = mock.MagicMock()
+    permissions = IsAdmin()
+
+    mock_request.user.is_staff = is_admin
+
+    assert permissions.has_object_permission(mock_request, mock_view, mock_object) is is_admin
+
+
+@pytest.mark.parametrize('permission', [
+    IsOwner,
+    IsAdmin
+],
+    ids=['IsOwner', 'IsAdmin'])
+@pytest.mark.parametrize('is_authenticated', [
+    True,
+    False
+],
+    ids=['is authenticated', 'is not authenticated'])
+def test_has_permission(permission, is_authenticated):
+    mock_request = mock.MagicMock()
+    mock_view = mock.MagicMock()
+    permissions = permission()
+
+    mock_request.user.is_authenticated = is_authenticated
+
+    assert permissions.has_permission(mock_request, mock_view) is is_authenticated
